c2bb7c08bb3f21fb695a04cca3edc413ae812a1f
Evelyne Lachance <eslachance@gmail.com>
2016-09-26 08:28:45 -0400

    Attempting to use http-kit for post instead. getting no more lucky.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dithcord "0.2.0-SNAPSHOT"
+(defproject dithcord "0.0.8"
   :description "A Discord Bot Library written in Clojure (which is a lisp, get it?)"
   :url "https://github.com/eslachance/dithcord"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.385"]
                  [cheshire "5.6.3"]
-                 [http.async.client "1.2.0"]]
+                 [http.async.client "1.2.0"]
+                 [http-kit "2.1.18"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
